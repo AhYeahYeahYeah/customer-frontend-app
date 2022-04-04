f81d6308ad57d6759002aa2877b9c90ab2242dc5
ambrosecm <96902760+ambrosecm@users.noreply.github.com>
2022-04-04 23:21:58 +0800

    product

--- pages/tabbar/product/products.vue
+++ pages/tabbar/product/products.vue
@@ -4,22 +4,12 @@
 				<input class="uni-input" name="input" placeholder="请输入" />
 				<button type="default">搜索</button>
 		</view>
-		<button type="default">智能客服</button>
+		<uni-section title="基本用法" type="line">
+			<view class="example-body">
+				<uni-combox :candidates="candidates" placeholder="请选择产品类型" v-model="city"></uni-combox>
+			</view>
+		</uni-section>
 	<view class="content">
-		<view class="uni-margin-wrap">
-			<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
-				<swiper-item>
-					<image src="../../../static/c1.png" mode=""></image>
-				</swiper-item>
-				<swiper-item>
-					<image src="../../../static/c2.png" mode=""></image>
-				</swiper-item>
-				<swiper-item>
-					<image src="../../../static/c3.png" mode=""></image>
-				</swiper-item>
-			</swiper>
-		</view>
-		<button type="default" v-on:click="moreProducts()">更多</button>
 		<view class="uni-list">
 			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in products" :key="index" v-on:click="productinfo(item.pid)">
 				<view class="uni-product-list">
@@ -37,10 +27,6 @@
 	export default {
 		data() {
 			return {
-				indicatorDots: true,
-				autoplay: true,
-				interval: 2000,
-				duration: 500,
 				products:[]
 			}
 		},
@@ -59,12 +45,8 @@
 				uni.navigateTo({
 					url:'../productinfo/productinfo?pid='+e
 				})
-			},
-			moreProducts(){
-				uni.navigateTo({
-					url:'./products'
-				})
 			}
+			
 		}
 	}
 </script>
---
+++ pages/tabbar/productinfo/productinfo.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<text>{{this.product[0]}}</text>
+		<button type="default" v-on:click="buy">购买</button>
+	</view>
+</template>
+
+<script>
+	import {EntityApi} from "../../../api/restful.js";
+	export default {
+		data() {
+			return {
+				product:[]
+			}
+		},
+		onLoad:function(e) {
+			console.log(e);
+			uni.showLoading({
+				title:"加载中..."
+			})
+			new EntityApi().getProduct(e.pid).then((res)=>{
+				this.product=res.data;
+				console.log(this.product[0]);
+				uni.hideLoading();
+			})
+		},
+		methods: {
+			buy(){
+				
+			}
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
