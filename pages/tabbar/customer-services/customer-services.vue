41621bc7ee280b0e9394c5b05fa390b3a94f8ea9
1010353663 <1010353663@qq.com>
2022-04-06 10:25:52 +0800

    add services

--- pages/tabbar/customer-services/customer-services.vue
+++ pages/tabbar/customer-services/customer-services.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		123
+		<uni-card @click="onClick" style="max-width: 70%;align-items: flex-end;border-radius: 30rpx;">
+			<text class="uni-body">这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+		</uni-card>
+		<uni-card @click="onClick" style="max-width: 70%;align-items: flex-end;border-radius: 30rpx;">
+			<text class="uni-body" style="max-width: 70%;">这是片。</text>
+		</uni-card>
 	</view>
 </template>
 
@@ -8,11 +13,11 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
